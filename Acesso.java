b0b029b27abf52df41cc0190f4b2e169dbe0beba
jpbusche <joaopaulo.busche@gmail.com>
2017-05-03 17:08:07 -0300

    Method: Divide temp variable
    Class Afected: CalculaHorario.java
    
    Divide the variable valorTotal in 3(valorTotal, valorTotalHoras, valorTotalMinutes) and transform temp variables quantidadeHoras and quantidadeMinutos in atributtes
    
    Signed-off-by: jpbusche <joaopaulo.busche@gmail.com>

--- Acesso.java
+++ Acesso.java
@@ -50,35 +50,43 @@ class CalculaHorario {
 	public static final float VALOR_HORA = (float) 7.0;
 	public static final float VALOR_DIARIA = (float) 30.0;
 
+	public int quantidadeHoras;
+	public int quantidadeMinutos;
+
 	public CalculaHorario() {}
 
 	public float calcularValor(int horaEntrada, int horaSaida, int minutosEntrada, int minutosSaida) {
-		int totalHoras = horaSaida - horaEntrada; 
-		
-		if(totalHoras >= 9)
+		int totalHoras = horaSaida - horaEntrada;
+		int totalMinutos = minutosSaida - minutosEntrada;
+
+		if(totalHoras >= 9) {
 			return VALOR_DIARIA;
-		
-		if (horaSaida == horaEntrada)
-			quantidadeMinutos = minutosSaida - minutosEntrada;
-		else if (horaSaida > horaEntrada && minutosEntrada == minutosSaida){
-			quantidadeMinutos = 0;
-			quantidadeHoras = horaSaida - horaEntrada;
-		}
-		else if (horaSaida > horaEntrada && minutosEntrada > minutosSaida) 
-			quantidadeMinutos = minutosSaida - minutosEntrada;
-		else if (horaSaida > horaEntrada && minutosSaida < minutosEntrada){
-			quantidadeMinutos = minutosSaida + (60 - minutosEntrada);
-			quantidadeHoras = horaSaida - horaEntrada - 1;
-		}
-		else {
-			quantidadeHoras = 0;
-			quantidadeMinutos = 0;
-		}
-		
-		float valorTotalHoras = quantidadeHoras * VALOR_HORA;
-		float valorTotalMinutos = (float) Math.ceil(quantidadeMinutos / 15.0) * VALOR_FRACAO;		
-		float valorTotal = valorTotalHoras + valorTotalMinutos; 
+		} else {
+			if (totalHoras == 0) {
+				quantidadeMinutos = totalMinutos;
+			}
+			else if (totalHoras > 0 && totalMinutos == 0) {
+				quantidadeMinutos = totalMinutos;
+				quantidadeHoras = totalHoras;
+			}
+			else if (totalHoras > 0 && totalMinutos > 1) {
+				quantidadeMinutos = totalMinutos;
+				quantidadeHoras = totalHoras;
+			}
+			else if (totalHoras > 0 && totalMinutos < 0){
+				quantidadeMinutos = minutosSaida + (60 - minutosEntrada);
+				quantidadeHoras = totalHoras - 1;
+			}
+			else {
+				quantidadeHoras = 0;
+				quantidadeMinutos = 0;
+			}
 			
-		return valorTotal;
+			float valorTotalHoras = quantidadeHoras * VALOR_HORA;
+			float valorTotalMinutos = (float) Math.ceil(quantidadeMinutos / 15.0) * VALOR_FRACAO;		
+			float valorTotal = valorTotalHoras + valorTotalMinutos; 
+				
+			return valorTotal;
+		}
 	}
 }
